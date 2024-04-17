a4bdd29f94e0e81ae6aa32bf216d5318319deb2d
Murdochjaime <161724266+Murdochjaime@users.noreply.github.com>
2024-04-17 22:11:24 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,8 +1,12 @@
 <footer class="container-fluid text-center">
 <div class="container">
 <div class="row">
-<div class="col-md-6">
-  <a class="navbar-brand" href="#"><span class="josh">JOSH </span> <span class="orange">WHITKIN</span></a>
+<div class="col-md-6 widgetleft">
+  <!-- <a class="navbar-brand" href="#"><span class="josh">JOSH </span> <span class="orange">WHITKIN</span></a> -->
+  <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-left') ) : 
+endif; ?>
+
+
 
 </div>
 
